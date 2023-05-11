4e33a3840264ef2d13639c81270a328f018e1457
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2023-05-11 15:05:58 +0100

    Improved formatting

--- conway.py
+++ conway.py
@@ -8,7 +8,7 @@ pygame.init()
 
 # Create a window where stuff can happen
 
-my_screen = pygame.display.set_mode( ( 800 , 600 ) , 5 )
+my_screen = pygame.display.set_mode((800, 600), 5)
 
 # Our grid's width and height
 
@@ -25,40 +25,40 @@ grid_y = 0
 
 # Colour of the boxes on the grid
 
-dead_color = ( 100 , 100 , 100 ) # Colour for Dead
-alive_color = (   0 ,  50 ,   0 ) # Colour for Alive
+dead_color = (100, 100, 100)  # Colour for Dead
+alive_color = (0, 50, 0)  # Colour for Alive
 
 # Create a specific pattern. This is a "glider".
 
 grid = [
-            [ 0 , 1 , 0 ] ,
-            [ 1 , 1 , 1 ] ,
-            [ 0 , 1 , 1 ]
-        ]
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 1, 1]
+]
 
 # This bit makes sure that our grid is the right size
 
-if len( grid ) > grid_height:
-    grid_height = len( grid )
+if len(grid) > grid_height:
+    grid_height = len(grid)
 for row in grid:
-    if len( row ) > grid_width:
-        grid_width = len( row )
+    if len(row) > grid_width:
+        grid_width = len(row)
 
-if len( grid ) < grid_height:
-    grid+= [ [] for i in range( grid_height - len( grid ) ) ]
+if len(grid) < grid_height:
+    grid += [[] for i in range(grid_height - len(grid))]
 
 for row in grid:
-    if len( row ) < grid_width:
-        row+= [ 0 for i in range( grid_width - len( row ) ) ]
+    if len(row) < grid_width:
+        row += [0 for i in range(grid_width - len(row))]
 
 # How to find the neighbours of a cell...
 
 neighbor_list = []
 
-for j in [ -1 , 0 , 1 ]:
-    for i in [ -1 , 0 , 1 ]:
-        if not( i == 0 and j == 0 ):
-            neighbor_list.append( ( i , j ) )
+for j in [-1, 0, 1]:
+    for i in [-1, 0, 1]:
+        if not (i == 0 and j == 0):
+            neighbor_list.append((i, j))
 
 # This creates a loop that only ends when you close the window
 
@@ -74,27 +74,28 @@ while still_looping:
 
     # Clear the screen
 
-    my_screen.fill( ( 100 , 100 , 150 ) )
+    my_screen.fill((100, 100, 150))
 
     # Create a working copy of our grid so that we can iterate
 
-    ref_grid = copy.deepcopy( grid )
+    ref_grid = copy.deepcopy(grid)
 
     # Now let's iterate
 
-    for y in range( grid_height ):
-        for x in range( grid_width ):
+    for y in range(grid_height):
+        for x in range(grid_width):
 
             # Start by displaying the current state
 
-            my_cell = grid[ y ][ x ]
+            my_cell = grid[y][x]
 
             if my_cell == 0:
                 my_color = dead_color
             if my_cell == 1:
                 my_color = alive_color
 
-            pygame.draw.rect( my_screen , my_color , ( grid_x + box_width * x , grid_y + box_height * y , box_width , box_height ) , 0 )
+            pygame.draw.rect(my_screen, my_color,
+                             (grid_x + box_width * x, grid_y + box_height * y, box_width, box_height), 0)
 
             # Counter for neighbours
 
@@ -102,7 +103,7 @@ while still_looping:
 
             # Work through neighbours
 
-            for ( i , j ) in neighbor_list:
+            for (i, j) in neighbor_list:
                 neighbor_x = x + i
                 neighbor_y = y + j
 
@@ -111,12 +112,12 @@ while still_looping:
                 if neighbor_x >= 0 and neighbor_y >= 0 and neighbor_x < grid_width and neighbor_y < grid_height:
 
                     # If so then count whether they are alive...
-                    if ref_grid[ neighbor_y ][ neighbor_x ] > 0:
+                    if ref_grid[neighbor_y][neighbor_x] > 0:
                         neighbor_count = neighbor_count + 1
 
             # Get the current cell value
 
-            cell_val = ref_grid[ y ][ x ]
+            cell_val = ref_grid[y][x]
 
             # Then iterate according to Conway's rules
 
@@ -141,12 +142,12 @@ while still_looping:
 
                 # ...with  more than three neighbours then die
 
-                if neighbor_count >3:
+                if neighbor_count > 3:
                     cell_val = 0
 
             # Update the grid
 
-            grid[ y ][  x ] = cell_val
+            grid[y][x] = cell_val
 
     # Show off our handiwork
 
@@ -154,7 +155,7 @@ while still_looping:
 
     # Take a breath to see what is going on
 
-    #time.sleep( 0.5 )
+    # time.sleep( 0.5 )
 
 # Finish things off
 pygame.quit()
--- zombie.py
+++ zombie.py
@@ -1,4 +1,4 @@
-import copy , time , pygame , sys , random
+import copy, time, pygame, sys, random
 
 # Get things up and running
 
@@ -6,11 +6,11 @@ pygame.init()
 
 # Create a window where stuff can happen
 
-my_screen = pygame.display.set_mode( ( 800 , 600 ) , 5 )
+my_screen = pygame.display.set_mode((800, 600), 5)
 
 # Load a font so that we can write something
 
-#my_font = pygame.font.SysFont( 'monospace' , 36 )
+# my_font = pygame.font.SysFont( 'monospace' , 36 )
 
 # Our grid's width and height
 
@@ -27,10 +27,10 @@ g_y = 0
 
 # Colour of the boxes on the grid
 
-r_col = (  10 ,  10 ,  10 ) # Colour for Removed
-s_col = (   0 ,   0 , 200 ) # Colour for Susceptible
-i_col = ( 200 ,   0 , 100 ) # Colour for Infectious
-w_col = ( 100 , 100 , 100 ) # Colour for Wall
+r_col = (10, 10, 10)  # Colour for Removed
+s_col = (0, 0, 200)  # Colour for Susceptible
+i_col = (200, 0, 100)  # Colour for Infectious
+w_col = (100, 100, 100)  # Colour for Wall
 
 # 1/Beta, 1/gamma and mu are R, the infectious period and recovery rate of our disease
 
@@ -46,58 +46,58 @@ grid = []
 
 # This bit makes sure that our grid is the right size
 
-if len( grid ) > h:
-    h = len( grid )
+if len(grid) > h:
+    h = len(grid)
 for row in grid:
-    if len( row ) > w:
-        w = len( row )
+    if len(row) > w:
+        w = len(row)
 
-if len( grid ) < h:
-    grid+= [ [] for i in range( h - len( grid ) ) ]
+if len(grid) < h:
+    grid += [[] for i in range(h - len(grid))]
 
 for row in grid:
-    if len( row ) < w:
-        row+= [ 1 for i in range( w - len( row ) ) ]
+    if len(row) < w:
+        row += [1 for i in range(w - len(row))]
 
 # Create a firewall
 
-f_x = int( w / 3 )
+f_x = int(w / 3)
 
 for row in grid:
-    row[ f_x ] = 3
+    row[f_x] = 3
 
 # Now leave some holes
 
 porosity = 0.03
-num_holes = int( h * porosity )
+num_holes = int(h * porosity)
 hole_count = 0
 
 while hole_count < num_holes:
-    hole_y = random.randint( 0 , h - 1 )
-    if grid[ hole_y ][ f_x ] == 3:
-        grid[ hole_y ][ f_x ] = 1
+    hole_y = random.randint(0, h - 1)
+    if grid[hole_y][f_x] == 3:
+        grid[hole_y][f_x] = 1
         hole_count += 1
 
 # Infect a few random people
 
 to_infect = 10
 
-for i in range( to_infect ):
+for i in range(to_infect):
     cell_available = False
-    while not( cell_available ):
-        x = random.randint( int( w / 2 ) , w - 1 )
-        y = random.randint( int( h / 2 ) , h - 1 )
-        cell_available = ( grid[ y ][ x ] == 1 )
-    grid[ y ][ x ] = 2
+    while not cell_available:
+        x = random.randint(int(w / 2), w - 1)
+        y = random.randint(int(h / 2), h - 1)
+        cell_available = (grid[y][x] == 1)
+    grid[y][x] = 2
 
 # How to find the neighbours of a cell...
 
 n_list = []
 
-for j in [ -1 , 0 , 1 ]:
-    for i in [ -1 , 0 , 1 ]:
-        if not( i == 0 and j == 0 ):
-            n_list.append( ( i , j ) )
+for j in [-1, 0, 1]:
+    for i in [-1, 0, 1]:
+        if not (i == 0 and j == 0):
+            n_list.append((i, j))
 
 # This creates a loop that only ends when you close the window
 
@@ -113,7 +113,7 @@ while still_looping:
 
     # Clean the board before we start!!
 
-    my_screen.fill( ( 100 , 100 , 150 ) )
+    my_screen.fill((100, 100, 150))
 
     # Reset counters for the state of the system
 
@@ -123,16 +123,16 @@ while still_looping:
 
     # Create a working copy of our grid so that we can iterate
 
-    ref_grid = copy.deepcopy( grid )
+    ref_grid = copy.deepcopy(grid)
 
     # Now let's iterate
 
-    for y in range( h ):
-        for x in range( w ):
+    for y in range(h):
+        for x in range(w):
 
             # Start by displaying the current state
 
-            cell_val = ref_grid[ y ][ x ]
+            cell_val = ref_grid[y][x]
 
             if cell_val == 0:
                 my_col = r_col
@@ -143,7 +143,7 @@ while still_looping:
             if cell_val == 3:
                 my_col = w_col
 
-            pygame.draw.rect( my_screen , my_col , ( g_x + b_width * x , g_y + b_height * y , b_width , b_height ) , 0 )
+            pygame.draw.rect(my_screen, my_col, (g_x + b_width * x, g_y + b_height * y, b_width, b_height), 0)
 
             # Counter for neighbours
 
@@ -151,7 +151,7 @@ while still_looping:
 
             # Work through neighbours
 
-            for ( i , j ) in n_list:
+            for (i, j) in n_list:
                 n_x = x + i
                 n_y = y + j
 
@@ -160,7 +160,7 @@ while still_looping:
                 if n_x >= 0 and n_y >= 0 and n_x < w and n_y < h:
 
                     # If so then count whether they are infected
-                    if ref_grid[ n_y ][ n_x ] == 2:
+                    if ref_grid[n_y][n_x] == 2:
                         n_count = n_count + 1
 
             # Then iterate according to our rules
@@ -174,7 +174,7 @@ while still_looping:
 
             # If alive with an infected neighbour then roll a die to infect
 
-            if cell_val == 1 and n_count >0:
+            if cell_val == 1 and n_count > 0:
                 S += 1
                 if random.random() < beta:
                     cell_val = 2
@@ -188,7 +188,7 @@ while still_looping:
 
             # Update the grid
 
-            grid[ y ][  x ] = cell_val
+            grid[y][x] = cell_val
 
     # Show off our handiwork
 
@@ -196,9 +196,9 @@ while still_looping:
 
     # Remind us of the values of the parameters and the current state
 
-    print( '\n\n beta = ' , beta , '\t\t S = ' , S)
-    print( 'gamma = ' , gamma , '\t\t I = ' , I )
-    print( '   mu = ' , mu , '\t\tR = ', R , '\n\n' )
+    # print('\n\n beta = ', beta, '\t\t S = ', S)
+    # print('gamma = ', gamma, '\t\t I = ', I)
+    # print('   mu = ', mu, '\t\tR = ', R, '\n\n')
 
 # Finish things off
 pygame.quit()
